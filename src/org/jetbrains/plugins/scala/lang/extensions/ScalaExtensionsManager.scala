a0ddd6221fdf117db8a6104f5be3908d3a44ffe0
Mikhail Mutcianko <mutcianko.m@gmail.com>
2017-02-16 18:01:54 +0300

    extensions: fix compilation

--- src/org/jetbrains/plugins/scala/lang/extensions/ScalaExtensionsManager.scala
+++ src/org/jetbrains/plugins/scala/lang/extensions/ScalaExtensionsManager.scala
@@ -3,8 +3,9 @@ package org.jetbrains.plugins.scala.lang.extensions
 import com.intellij.openapi.components.ProjectComponent
 import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
-import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScClass, ScTrait}
+import org.jetbrains.plugins.scala.lang.psi.api.base.types.ScParameterizedTypeElement
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScExpression, ScMethodCall}
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScClass, ScObject, ScTrait}
 
 import scala.meta._
 
@@ -26,10 +27,14 @@ object ScalaExtensionsManager {
   def getInstance(project: Project): ScalaExtensionsManager = project.getComponent(classOf[ScalaExtensionsManager])
 
   val metaToPsi: Map[Class[_ <: Tree], Class[_ <: ScalaPsiElement]] = Map(
-    classOf[Tree] -> classOf[ScalaPsiElement],
-    classOf[Term] -> classOf[ScExpression],
-    classOf[Defn.Class] -> classOf[ScClass],
-    classOf[Defn.Trait] -> classOf[ScTrait],
-//    classOf[]
+    classOf[Tree]           -> classOf[ScalaPsiElement],
+    classOf[Term]           -> classOf[ScExpression],
+    classOf[Defn.Class]     -> classOf[ScClass],
+    classOf[Defn.Trait]     -> classOf[ScTrait],
+    classOf[Defn.Object]    -> classOf[ScObject],
+    classOf[Term.Apply]     -> classOf[ScMethodCall],
+    classOf[Term.ApplyType] -> classOf[ScParameterizedTypeElement]
   )
+
+  val psiToMeta: Map[Class[_ <: ScalaPsiElement], Class[_ <: Tree]] = metaToPsi.map(_.swap)
 }
