a0d9ce09b53c2c956e44631663d5ad69a7bb09a8
Mikhail Mutcianko <mutcianko.m@gmail.com>
2017-02-21 20:17:04 +0300

    extensions: add more stubs to extension manager

--- src/org/jetbrains/plugins/scala/components/extensions/ScalaExtensionsManager.scala
+++ src/org/jetbrains/plugins/scala/components/extensions/ScalaExtensionsManager.scala
@@ -2,6 +2,9 @@ package org.jetbrains.plugins.scala.components.extensions
 
 import com.intellij.openapi.components.ProjectComponent
 import com.intellij.openapi.project.Project
+import org.jetbrains.plugins.scala.components.extensions.api.ArbitraryTreeTransformer
+import org.jetbrains.plugins.scala.components.extensions.api.impl.IdeaApiProvider
+import org.jetbrains.plugins.scala.components.extensions.impl.KindProjectorExtension
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
 import org.jetbrains.plugins.scala.lang.psi.api.base.types.ScParameterizedTypeElement
 import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScExpression, ScMethodCall}
@@ -12,14 +15,28 @@ import scala.meta._
 class ScalaExtensionsManager(project: Project) extends ProjectComponent {
   import ScalaExtensionsManager._
 
-  override def projectOpened(): Unit = ()
+  override def projectOpened(): Unit = loadExtensions()
   override def projectClosed(): Unit = ()
   override def initComponent(): Unit = ()
   override def disposeComponent(): Unit = ()
   override def getComponentName: String = "ScalaExtensionsManager"
 
-  def getExtensions: Seq[ScalaPluginExtension] = ???
+  def getExtensions: Seq[ScalaPluginExtension] = Seq(new KindProjectorExtension(new IdeaApiProvider(project)))
   def getApplicableExtensions(elem: ScalaPsiElement): Seq[ScalaPluginExtension] = getExtensions
+  def getTransformersForElement(elem: ScalaPsiElement): Seq[ArbitraryTreeTransformer] = {
+    transformersByContext.getOrElse(psiToMeta.getOrElse(elem.getClass, classOf[Tree]), Seq.empty)
+  }
+
+  private def loadExtensions(): Unit = {
+    for {
+      extension <- getExtensions
+      tr <- extension.transformers
+    } {
+      transformersByContext(tr.context.elementType) = transformersByContext.getOrElse(tr.context.elementType, Seq.empty)
+    }
+  }
+
+  private val transformersByContext: scala.collection.mutable.Map[Class[_ <: Tree], Seq[ArbitraryTreeTransformer]] = Map()
 
 }
 
