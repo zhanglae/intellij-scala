467c86f6d57a7ceaba00c453b4d03e7491ce542b
Mikhail Mutcianko <mutcianko.m@gmail.com>
2017-02-21 20:16:48 +0300

    extensions: refactor extension base class

--- src/org/jetbrains/plugins/scala/components/extensions/ScalaPluginExtension.scala
+++ src/org/jetbrains/plugins/scala/components/extensions/ScalaPluginExtension.scala
@@ -1,24 +1,21 @@
 package org.jetbrains.plugins.scala.components.extensions
 
 import org.jetbrains.plugins.scala.components.extensions.api.base.MigrationDescriptor
-import org.jetbrains.plugins.scala.components.extensions.api.{BaseApiProvider, InspectionConverter, ProjectMigrator, ArbitraryTreeConverter}
+import org.jetbrains.plugins.scala.components.extensions.api.{BaseApiProvider, InspectionConverter, ProjectMigrator, ArbitraryTreeTransformer}
 
 /**
   * @author mutcianm
   * @since 09.02.17.
   */
-abstract class ScalaPluginExtension {
-  def version: Int
+abstract class ScalaPluginExtension (protected val api: BaseApiProvider) {
+//  def version: Int
   def extensionName: String
   def extensionDescription: String
   // TODO: compatible libraries?
 
-  /* Platform API entrypoint for developers */
-  def api: BaseApiProvider
-
   /* Exported extensions */
-  def inspections:  Seq[InspectionConverter]
-  def migrators(migrationDescriptor: MigrationDescriptor): Seq[ProjectMigrator]
-  def transformers: Seq[ArbitraryTreeConverter]
+  def inspections:  Seq[InspectionConverter] = Seq.empty
+  def migrators(migrationDescriptor: MigrationDescriptor): Seq[ProjectMigrator] = Seq.empty
+  def transformers: Seq[ArbitraryTreeTransformer] = Seq.empty
   // TODO: other extensions
 }
